c7df3f2b4611c4e596e4a5ccc65ee8fd6bdcde05
EtiamSani <etiam.sani@laposte.net>
2023-12-11 23:05:37 +0100

    user signup form, styling form

--- components/usersSignup/UsersSignUpForm.tsx
+++ components/usersSignup/UsersSignUpForm.tsx
@@ -24,10 +24,10 @@ const UsersLoginForm = ({ className, ...props }:any) => {
             <div className='bg-black w-full hidden lg:block lg:h-screen'>
               <div className="relative h-full w-full bg-black">
                 <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-                  <h1 className='text-white grid justify-items-center text-7xl font-bold text-center mt-10'>EIP Portfolio</h1>
+                  <h1 className='text-white grid justify-items-center text-7xl font-bold text-center mt-10'><span className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text">EIP </span> Portfolio</h1>
                 </div>
-                <div className="absolute left-0 right-0 bottom-5 text-center text-white">
-                  <h2 className='text-2xl'>Suivez dès maintenant mes investissement PEA et CTO</h2>
+                <div className="absolute left-0 right-0 bottom-12 text-center text-white">
+                  <h2 className='text-3xl font-bold'>Suivez dès maintenant <span className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text">tout</span> mes investissements <br/> PEA et CTO <span className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text">en temps réel ! </span></h2>
                 </div>
               <div className="absolute left-0 right-0 top-[-10%] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]">
               </div>
