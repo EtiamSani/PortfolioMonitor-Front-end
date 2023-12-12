033e994897fefa42f20fa6478a1228ee7dddca16
EtiamSani <etiam.sani@laposte.net>
2023-12-12 15:17:52 +0100

    payment page, payement component & styling

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -5,8 +5,9 @@ const page = () => {
     const priceCardsData = [
         {
             title: 'Portefeuilles EIP*',
-            description: 'Best option for personal use & for your next project.',
+            description: 'Suivez l’évolution de mes portefeuilles en temps réel avec mise à jour de mes lignes immédiatement après chaque transaction.',
             price: '60€',
+            temporality: '/an',
             items: [
                 'Accès aux portefeuilles PEA et CTO en temps réel',
                 'Mise à jour de mes Google Sheets immédiatement après chaque transaction',
@@ -14,24 +15,55 @@ const page = () => {
                 'Compte-rendu mensuel avec récapitulatif des transactions ',
                 'Suivi des positions en portefeuille et partage de mes réflexions ',
             ],
+            itemsNotInTheOffer: [
+                'Accès watchlist',
+                'Analyses d\'actions (2 par mois)',
+                'Discord privé',
+                'Réunions (2 par mois)'
+            ],
+            
         },
         {
             title: 'Portefeuilles EIP*',
-            description: 'Best option for personal use & for your next project.',
-            price: '60€',
+            description: 'Suivez l’évolution de mes portefeuilles en temps réel avec mise à jour de mes lignes immédiatement après chaque transaction.',
+            price: '90€',
+            temporality: 'pour 3 mois',
             items: [
                 'Accès aux portefeuilles PEA et CTO en temps réel',
                 'Mise à jour de mes Google Sheets immédiatement après chaque transaction',
                 'Alerte transactions ',
+                'Accès watchlist',
+                'Analyses d\'actions (2 par mois)',
                 'Compte-rendu mensuel avec récapitulatif des transactions ',
+                'Discord privé',
                 'Suivi des positions en portefeuille et partage de mes réflexions ',
+                'Réunions (2 par mois)'
             ],
+            
+        },
+        {
+            title: 'Team EIP',
+            description: 'Interagissez avec moi dans le Discord privé. Partagez Des idées d’investissement qui pourront être analysées en détail lors des réunions.',
+            price: '300€',
+            temporality: '/an',
+            items: [
+                'Accès aux portefeuilles PEA et CTO en temps réel',
+                'Mise à jour de mes Google Sheets immédiatement après chaque transaction',
+                'Alerte transactions ',
+                'Accès watchlist',
+                'Analyses d\'actions (2 par mois)',
+                'Compte-rendu mensuel avec récapitulatif des transactions ',
+                'Discord privé',
+                'Suivi des positions en portefeuille et partage de mes réflexions ',
+                'Réunions (2 par mois)'
+            ],
+            cardBorderClassName:'flex flex-col p-1 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg bg-gradient-to-r from-blue-500 to-purple-500'
         },
     ];
   return (
     <div className="absolute top-0 left-0 flex items-center justify-center right-0 z-[-2] h-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex flex-col items-center"> {/* Ajout d'une largeur maximale */}
-        <div className="space-y-8 lg:grid lg:grid-cols-2 sm:gap-6 xl:gap-10 lg:space-y-0">
+        <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
             {priceCardsData.map((cardData, index) => (
                 <PriceCard
                     key={index}
@@ -39,6 +71,9 @@ const page = () => {
                     description={cardData.description}
                     price={cardData.price}
                     items={cardData.items}
+                    itemsNotInTheOffer={cardData.itemsNotInTheOffer}
+                    temporality={cardData.temporality}
+                    cardBorderClassName={cardData.cardBorderClassName}
                 />
             ))}
         </div>
--- components/pricing/PriceCard.tsx
+++ components/pricing/PriceCard.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Card } from '../ui/card';
 import { Button } from '@/components/ui/button';
 
-const PriceCard = ({ title, description, price, items }:any) => {
+const PriceCard = ({ title, description, price, items, temporality, itemsNotInTheOffer, cardBorderClassName }:any) => {
     return (
+        <div className={cardBorderClassName}>
         <Card className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border">
             <h3 className="mb-4 text-2xl font-semibold">{title}</h3>
             <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">{description}</p>
             <div className="flex justify-center items-baseline my-8">
                 <span className="mr-2 text-5xl font-extrabold">{price}</span>
-                <span className="text-gray-500 dark:text-gray-400">/an</span>
+                <span className="text-gray-500 dark:text-gray-400">{temporality}</span>
             </div>
             {/* List */}
             <ul role="list" className="mb-8 space-y-4 text-left">
@@ -20,9 +21,17 @@ const PriceCard = ({ title, description, price, items }:any) => {
                         <span>{item}</span>
                     </li>
                 ))}
+                {itemsNotInTheOffer && itemsNotInTheOffer.map((itemNotInTheOffer:any, index:any) => (
+                    <li key={index} className="flex items-center space-x-3">
+                        {/* Croix */}
+                        <svg className="flex-shrink-0 w-5 h-5 text-red-500 dark:text-red-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M13.414 6.586a2 2 0 112.828 2.828L11.828 12l4.414 4.414a2 2 0 11-2.828 2.828L9 14.828l-4.414 4.414a2 2 0 11-2.828-2.828L6.172 12 1.758 7.586a2 2 0 112.828-2.828L9 9.172l4.414-4.414z" clipRule="evenodd"></path></svg>
+                        <span>{itemNotInTheOffer}</span>
+                    </li>
+                ))}
             </ul>
             <Button>Accéder au portefeuilles</Button>
         </Card>
+        </div>
     );
 };
 
