e39d0444210c0df89cf719179ac1823490804e26
EtiamSani <etiam.sani@laposte.net>
2023-12-12 17:40:14 +0100

    payment page, payement component & styling

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -5,12 +5,12 @@ const page = () => {
     const priceCardsData = [
         {
             title: 'Portefeuilles EIP*',
-            description: 'Suivez l’évolution de mes portefeuilles en temps réel avec mise à jour de mes lignes immédiatement après chaque transaction.',
+            description: 'Suivez l’évolution de mes portefeuilles en temps réel.',
             price: '60€',
             temporality: '/an',
             items: [
-                'Accès aux portefeuilles PEA et CTO en temps réel',
-                'Mise à jour de mes Google Sheets immédiatement après chaque transaction',
+                'PEA et CTO en temps réel',
+                'Mise à jour immédiatement après chaque transaction',
                 'Alerte transactions ',
                 'Compte-rendu mensuel avec récapitulatif des transactions ',
                 'Suivi des positions en portefeuille et partage de mes réflexions ',
@@ -21,51 +21,58 @@ const page = () => {
                 'Discord privé',
                 'Réunions (2 par mois)'
             ],
-            buttonTexts: 'Accèdez à mes portefeuilles'
-            
-        },
-        {
-            title: 'Portefeuilles EIP*',
-            description: 'Suivez l’évolution de mes portefeuilles en temps réel avec mise à jour de mes lignes immédiatement après chaque transaction.',
-            price: '90€',
-            temporality: 'pour 3 mois',
-            items: [
-                'Accès aux portefeuilles PEA et CTO en temps réel',
-                'Mise à jour de mes Google Sheets immédiatement après chaque transaction',
-                'Alerte transactions ',
-                'Accès watchlist',
-                'Analyses d\'actions (2 par mois)',
-                'Compte-rendu mensuel avec récapitulatif des transactions ',
-                'Discord privé',
-                'Suivi des positions en portefeuille et partage de mes réflexions ',
-                'Réunions (2 par mois)'
-            ],
-            buttonTexts: 'Rejoindre la Team'
+            buttonTexts: 'Accèdez à mes portefeuilles',
+            classNameForPrice:'mr-2 text-5xl font-extrabold '
             
         },
         {
             title: 'Team EIP',
-            description: 'Interagissez avec moi dans le Discord privé. Partagez Des idées d’investissement qui pourront être analysées en détail lors des réunions.',
+            description: 'Rejoignez mon équipe d\'investisseurs particuliers !',
+            price: '90€',
+            temporality: 'pour 3 mois',
+            items: [
+                'PEA et CTO en temps réel',
+                'Mise à jour immédiatement après chaque transaction',
+                'Alerte transactions',
+                'Compte-rendu mensuel avec récapitulatif des transactions ',
+                'Suivi des positions en portefeuille et partage de mes réflexions ',
+                'Accès watchlist',
+                'Analyses d\'actions (2 par mois)',
+                'Discord privé',
+                'Réunions (2 par mois)'
+            ],
+            buttonTexts: 'Rejoindre la Team',
+            classNameForPrice:'mr-2 text-5xl font-extrabold '
+            
+        },
+        {
+            title: 'Team EIP',
+            description: 'Rejoignez mon équipe d\'investisseurs particuliers !',
             price: '300€',
             temporality: '/an',
             items: [
-                'Portefeuilles PEA et CTO en temps réel',
-                'Mise à jour de mes Google Sheets immédiatement après chaque transaction',
-                'Alerte transactions ',
+                'PEA et CTO en temps réel',
+                'Mise à jour immédiatement après chaque transaction',
+                'Alerte transactions',
+                'Compte-rendu mensuel avec récapitulatif des transactions ',
+                'Suivi des positions en portefeuille et partage de mes réflexions ',
                 'Accès watchlist',
                 'Analyses d\'actions (2 par mois)',
-                'Compte-rendu mensuel avec récapitulatif des transactions ',
                 'Discord privé',
-                'Suivi des positions en portefeuille et partage de mes réflexions ',
                 'Réunions (2 par mois)'
             ],
             cardBorderClassName:'flex flex-col p-1 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg bg-gradient-to-r from-blue-500 to-purple-500',
-            buttonTexts: 'Rejoindre la Team'
+            buttonTexts: 'Rejoindre la Team',
+            classNameForPrice:'mr-2 text-5xl font-extrabold bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text'
         },
     ];
   return (
     <div className="absolute top-0 left-0 flex items-center justify-center right-0 z-[-2] h-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex flex-col items-center h-screen md:mt-10"> {/* Ajout d'une largeur maximale */}
+                <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-8">
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Choissisez votre offre</h2>
+                <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Vous êtes déterminé(e) à prendre en main votre épargne, à investir en Bourse et à vous construire un patrimoine à long terme, et vous souhaitez être accompagné(e) ? Ces offres peuvent vous intéresser.</p>
+                </div>
             <div className="sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0 ">
                 {priceCardsData.map((cardData, index) => (
                     <div key={index} className="mb-8 ">
@@ -79,6 +86,8 @@ const page = () => {
                         temporality={cardData.temporality}
                         cardBorderClassName={cardData.cardBorderClassName}
                         buttonTexts={cardData.buttonTexts}
+                        classNameForPrice={cardData.classNameForPrice}
+                        
                     />
                     </div>
                 ))}
--- components/pricing/PriceCard.tsx
+++ components/pricing/PriceCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card } from '../ui/card';
 import { Button } from '@/components/ui/button';
 
-const PriceCard = ({ title, description, price, items, temporality, itemsNotInTheOffer, cardBorderClassName, buttonTexts }:any) => {
+const PriceCard = ({ title, description, price, items, temporality, itemsNotInTheOffer, cardBorderClassName, buttonTexts, classNameForPrice }:any) => {
     return (
         
         <div className={cardBorderClassName}>
@@ -10,7 +10,7 @@ const PriceCard = ({ title, description, price, items, temporality, itemsNotInTh
             <h3 className="mb-4 text-2xl font-semibold">{title}</h3>
             <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">{description}</p>
             <div className="flex justify-center items-baseline my-8">
-                <span className="mr-2 text-5xl font-extrabold">{price}</span>
+                <span className={classNameForPrice}>{price}</span> 
                 <span className="text-gray-500 dark:text-gray-400">{temporality}</span>
             </div>
             {/* List */}
@@ -26,7 +26,7 @@ const PriceCard = ({ title, description, price, items, temporality, itemsNotInTh
                     <li key={index} className="flex items-center space-x-3">
                         {/* Croix */}
                         <svg className="flex-shrink-0 w-5 h-5 text-red-500 dark:text-red-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M13.414 6.586a2 2 0 112.828 2.828L11.828 12l4.414 4.414a2 2 0 11-2.828 2.828L9 14.828l-4.414 4.414a2 2 0 11-2.828-2.828L6.172 12 1.758 7.586a2 2 0 112.828-2.828L9 9.172l4.414-4.414z" clipRule="evenodd"></path></svg>
-                        <span>{itemNotInTheOffer}</span>
+                        <span className='text-gray-400'>{itemNotInTheOffer}</span>
                     </li>
                 ))}
             </ul>
