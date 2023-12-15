2e3f95d683e72e9ffeca101d258c05c317e4d2e3
EtiamSani <etiam.sani@laposte.net>
2023-12-15 17:32:34 +0100

    sidebar component, sidebar button component

--- components/navbar/Sidebar.tsx
+++ components/navbar/Sidebar.tsx
@@ -15,12 +15,12 @@ const Sidebar = () => {
       aria-label="Sidebar"
     >
       <div className="h-full px-3 py-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <h1 className="p-2 text-4xl bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text font-bold mb-10">
+        <h1 className="p-2 text-4xl bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text font-bold mb-2">
           EIP Portfolio{" "}
         </h1>
-        <Separator />
-        <div className="m-5">Bonjour jean</div>
-        <Separator className="mb-5" />
+        <Separator className="bg-gray-200" />
+        <div className="m-6 ">Bonjour jean</div>
+        <Separator className="mb-5 bg-gray-200" />
         menu
         <ul className="space-y-2 font-medium">
           {sidebarData.map((buttonData, index) => (
